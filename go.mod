f7ff4187baaa520013e76519a66ac6c85a7bc4e7
Steve Sklar <sklarsa@gmail.com>
2023-10-12 18:12:30 -0400

    Initial commit getting cgo working

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sklarsa/stock-prices-plugin
+
+go 1.21.2
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+// #cgo pkg-config: gtk+-3.0
+// #cgo CFLAGS: -I/usr/include/xfce4/libxfce4panel-2.0
+// #cgo LDFLAGS: -L/usr/lib/aarch64-linux-gnu -lxfce4panel-2.0
+// #include <libxfce4panel/libxfce4panel.h>
+import "C"
+
+func main() {
+	a := C.libxfce4panel_check_version(1, 2, 1)
+	println(*a)
+	println(C.XFCE_SCREEN_POSITION_NONE)
+	println(C.XFCE_SCREEN_POSITION_NW_H)
+
+}
+
+//export Construct
+func Construct(ptr *C.XfcePanelPluginFunc) {
+	println("hello I'm registering my plugin")
+}
